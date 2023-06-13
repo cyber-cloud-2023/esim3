44a4880d0a5d2f1bca3c89373ae51618bedd8f0d
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-06-13 09:59:58 +0300

    collection esimerkki valmis

---
+++ CollectionExample/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionExample
+{
+    internal class Person
+    {
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+
+        public Person(string fn, string ln)
+        {
+            Fname = fn;
+            Lname = ln;
+        }
+        public Person() { }
+    }
+}
---
+++ CollectionExample/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CollectionExample;
+
+Console.WriteLine("Hello, World!");
+
+List<Person> personCollection = new List<Person>();
+
+personCollection.Add(new Person("Teppo", "Testi"));
+personCollection.Add(new Person("Liisa", "Joki"));
+personCollection.Add(new Person("Aino", "Virta"));
+personCollection.Add(new Person("Matti", "Metsä"));
+
+Console.WriteLine("Listassa on "+ personCollection.Count + " henkilöä");
+Console.WriteLine("Ensimmäisen etunimi="+personCollection[0].Fname);
+Console.WriteLine("for-silmukalla nimet");
+for(int x = 0;  x < personCollection.Count; x++)
+{
+    Console.WriteLine(personCollection[x].Fname+" " + personCollection[x].Lname);
+}
+Console.WriteLine("foreach silmukalla nimet");
+foreach(Person onePerson in personCollection)
+{
+    Console.WriteLine(onePerson.Fname+ " "+onePerson.Lname);
+}
